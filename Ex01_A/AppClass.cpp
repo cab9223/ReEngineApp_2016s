24c118282777fa253133ca3a893dc4b2d1d2cdd4
ChrisBennett <cab9223@rit.edu>
2017-04-02 22:59:37 -0400

    Finished the EX01_A assignment

--- Ex01_A/AppClass.cpp
+++ Ex01_A/AppClass.cpp
@@ -15,6 +15,33 @@ void AppClass::Update(void)
 
 #pragma region YOUR CODE GOES HERE
 	modelMatrix = IDENTITY_M4;
+
+	//Set the percentage for the translation
+	static float fPercentageT = 0.0f;
+
+	//Set the quaternion rotation angles
+	quaternion qz1 = glm::angleAxis(0.0f, REAXISZ);
+	quaternion qz2 = glm::angleAxis(359.9f, REAXISZ);
+
+	//Map the timer with the amount of Z rotation
+	float fPercentageZ = MapValue(fTimer, 0.0f, 2.0f, 0.0f, 2.0f);
+
+	//Map the timer with the amount of translation depending on the timer
+	if (fTimer < 1.0) {
+		fPercentageT = MapValue(fTimer, 0.0f, 1.0f, 0.0f, 3.0f);
+	}
+	else if (fTimer < 2.0) {
+		fPercentageT = MapValue(fTimer, 1.0f, 2.0f, 3.0f, 0.0f);
+	}
+	else {
+		fTimer = 0;
+	}
+
+	//slerp the quaternions with by the percentage
+	quaternion rotZ = glm::mix(qz1, qz2, fPercentageZ);
+
+	//Multiply everything together for the modelMatrix
+	modelMatrix = glm::translate(modelMatrix, vector3(0.0f,fPercentageT, 0.0f)) *  glm::mat4_cast(rotZ);
 #pragma endregion
 
 #pragma region DOES NOT NEED CHANGES
